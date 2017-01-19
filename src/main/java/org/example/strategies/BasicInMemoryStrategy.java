71f238815761fb825e1b80dbd056eaa4a4b7eab9
Matti Tahvonen <matti@vaadin.com>
2017-01-19 18:54:14 +0200

    Converting Table + BeanItemContainer example to V8 Grid usage

--- src/main/java/org/example/strategies/BasicInMemoryStrategy.java
+++ src/main/java/org/example/strategies/BasicInMemoryStrategy.java
@@ -1,8 +1,7 @@
 package org.example.strategies;
 
-import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.Table;
+import com.vaadin.ui.Grid;
 import org.example.backend.GPSRouteService;
 import org.example.backend.Update;
 
@@ -15,9 +14,21 @@ public class BasicInMemoryStrategy implements TableStrategy {
 
     @Override
     public Component getTable() {
-        Table entryList = new Table();
-        BeanItemContainer<Update> container = new BeanItemContainer<>(Update.class,service.findAll());
-        entryList.setContainerDataSource(container);
+        Grid<Update> entryList = new Grid();
+        entryList.addColumn(u->u.getAltitude().toString()).setCaption("Altitude");
+        entryList.addColumn(u->u.getCharging().toString()).setCaption("Charging");
+        entryList.addColumn(u->u.getFixCount().toString()).setCaption("Fix Count");
+        entryList.addColumn(u->u.getLon().toString()).setCaption("Lon");
+        entryList.addColumn(u->u.getSpeed().toString()).setCaption("Speed");
+        entryList.addColumn(u->u.getSignalLevel().toString()).setCaption("Signal Level");
+        entryList.addColumn(u->u.getValid().toString()).setCaption("Valid");
+        entryList.addColumn(u->u.getCourse().toString()).setCaption("Course");
+        entryList.addColumn(u->u.getImei()).setCaption("Imei");
+        entryList.addColumn(u->u.getId() +"").setCaption("Id");
+        entryList.addColumn(u->u.getLat().toString()).setCaption("Lat");
+        entryList.addColumn(u->u.getBatteryLevel().toString()).setCaption("Battery Level");
+        entryList.addColumn(u->u.getTimestamp().toString()).setCaption("Timestamp");
+        entryList.setItems(service.findAll());
         entryList.setSizeFull();
         return entryList;
     }
